9cbbc4064f5491840b99313ad2b483471ddbbd4d
Rupantar Chakraborty <chakrabortyr2002@gmail.com>
2023-10-06 08:11:09 +0530

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,39 @@
+ import java.util.Scanner;
+
+    public class ex1 {
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter a: ");
+//        int a = sc.nextInt();
+//        System.out.print("Enter b: ");
+//        int b = sc.nextInt();
+//        System.out.print("Enter c: ");
+//        int c = sc.nextInt();
+
+
+            //q1) Sum of 3 numbers
+//        System.out.println("Sum of 3 numbers is "+ (a+b+c));
+            // if we write System.out.println("Sum of 3 numbers is "+ a+b+c); then output '123'
+
+            //q2) percentage using marks of 3 subjects
+//        System.out.println("Percentage is "+ (((a+b+c)*100)/300) +" %");
+
+            //q3) greeting with user input name
+//        System.out.println("Enter your name: ");
+//        String d = sc.nextLine();
+//        System.out.println("Hello! "+d);
+
+            //q4) kilometers to miles
+//        System.out.print("Enter distance in km: ");
+//        int e = sc.nextInt();
+//        System.out.println("Distance in miles is : "+ (e*0.62137119));
+
+            //q5) check user input is int or not
+            System.out.println("Enter an integer: ");
+            System.out.println(sc.hasNextInt());
+
+
+
+    }
+}
